d61654b24b289fa339675b30cf58a778cf6cf8d4
Johann <johanncareer@gmail.com>
2024-06-27 17:34:53 +0700

    Initial commit

---
+++ client/src/apollo.config.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, Provider } from '@angular/core';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+
+const uri = 'http://localhost:5006/graphql';
+
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  imports: [HttpClientModule, ApolloModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
